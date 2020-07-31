dbe839ac1c988bd7cbb1719f1d64df9ea844e311
nicolanzi1 <nicolanzi@gmail.com>
2020-07-31 09:54:41 +0100

    Created Likes polymorphic associations between users, artworks and comments, for users to like both comments and artworks, and to be able to GET users who have liked them

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -7,6 +7,7 @@ class Artwork < ApplicationRecord
     has_many :shared_viewers, through: :artwork_shares, source: :viewer
     has_many :comments, dependent: :destroy
     belongs_to :artist, foreign_key: :artist_id, class_name: "User"
+    has_many :likes, as: :likeable
 
     def self.artworks_for_user_id(user_id)
         Artwork
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     has_many :artwork_shares, foreign_key: :viewer_id, dependent: :destroy
     has_many :shared_artworks, through: :artwork_shares, source: :artwork
     has_many :comments, dependent: :destroy
+    has_many :likes
+    has_many :liked_comments, through: :likes, source: :likeable, source_type: 'Comment'
+    has_many :liked_artworks, through: :likes, source: :likeable, source_type: 'Artwork'
 end
